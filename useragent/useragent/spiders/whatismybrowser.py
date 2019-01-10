563e883a08c1038f542cea7672aeb23c07477315
5218664b <1141802674@qq.com>
2019-01-10 08:49:48 +0800

    添加新爬虫，对whatismybrowser.com上useragent的抓取

---
+++ useragent/useragent/spiders/whatismybrowser.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.link import Link
+from useragent.items import UseragentItem
+import re
+
+class WhatismybrowserSpider(CrawlSpider):
+    name = 'whatismybrowser'
+    allowed_domains = ['whatismybrowser.com']
+    start_urls = ['https://developers.whatismybrowser.com/useragents/explore/']
+
+    rules = [Rule(LinkExtractor(allow=('/useragents/explore/.+/.+/')),callback='parse_type', follow=False)]
+
+    def parse_type(self, response):
+        maxPage = response.xpath('//div[@id="pagination"]/a[last()]/@href').extract()[0].rsplit('/',1)[1]
+        for i in range(1,int(maxPage)):
+            link = response.urljoin(str(i)) 
+            yield scrapy.Request(
+                link
+                ,method='GET'
+                ,callback=self.parse_useragent
+            )            
+
+    def parse_useragent(self, response):
+        linkEx = LinkExtractor(restrict_xpaths='//td[@class="useragent"]/a')
+        links = linkEx.extract_links(response)
+        tmp = []
+        for link in links:
+            tmp.append(link.text)
+            print link.text
+        useragentItem = UseragentItem()
+        useragentItem['useragent'] = tmp
+        yield useragentItem
+
+
